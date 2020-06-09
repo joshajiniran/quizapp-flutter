9c91bc769bb0e307f94844077b841020f3066d00
Josh Ajiniran <josuajiniran@gmail.com>
2020-06-09 11:46:05 +0100

    Added alert feature on quiz completion and resetting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 import 'quizbrain.dart';
 
@@ -42,18 +43,59 @@ class _QuizAppPageState extends State<QuizAppPage> {
     bool correctAnswer = quizBrain.getQuestionAnswer();
 
     setState(() {
-      if (userChoice == correctAnswer) {
-        print('User is correct');
-        scorekeeper.add(
-          Icon(Icons.check, color: Colors.green),
-        );
+      if (quizBrain.isFinished() == true) {
+        var alert = Alert(
+            context: context,
+            type: AlertType.info,
+            title: "Quiz Completed",
+            desc:
+                "You have completed the quiz, your result is being compiled and will be sent to you",
+            style: AlertStyle(
+              animationType: AnimationType.fromTop,
+              isCloseButton: false,
+              isOverlayTapDismiss: false,
+              descStyle: TextStyle(fontWeight: FontWeight.bold),
+              animationDuration: Duration(milliseconds: 400),
+              alertBorder: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+                side: BorderSide(
+                  color: Colors.grey,
+                ),
+              ),
+              titleStyle: TextStyle(color: Colors.red),
+            ),
+            buttons: [
+              DialogButton(
+                child: Text(
+                  'Close',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () => Navigator.pop(context),
+                width: 150,
+                height: 50,
+              )
+            ]);
+
+        alert.show();
+        quizBrain.reset();
+        scorekeeper.clear();
       } else {
-        print('User is wrong');
-        scorekeeper.add(
-          Icon(Icons.close, color: Colors.red),
-        );
+        if (userChoice == correctAnswer) {
+          print('User is correct');
+          scorekeeper.add(
+            Icon(Icons.check, color: Colors.green),
+          );
+        } else {
+          print('User is wrong');
+          scorekeeper.add(
+            Icon(Icons.close, color: Colors.red),
+          );
+        }
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
     });
   }
 
--- lib/quizbrain.dart
+++ lib/quizbrain.dart
@@ -10,6 +10,31 @@ class QuizBrain {
         false),
     Question('An oxen is a male cow?', true),
     Question('The tallest building in the world is the Eiffel\'s tower?', true),
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
   ];
 
   String getQuestionText() {
@@ -24,5 +49,19 @@ class QuizBrain {
     if (_qIndex < _questions.length - 1) {
       _qIndex++;
     }
+  }
+
+  bool isFinished() {
+    if (_qIndex == _questions.length - 1) {
+      print('True');
+      return true;
+    } else {
+      print('False');
+      return false;
+    }
+  }
+
+  void reset() {
+    _qIndex = 0;
   }
 }
